2e7265831396a329c19c393391c7d2ecf997e2db
ademnsir <nsir.adem@esprit.tn>
2024-10-25 12:57:30 +0100

    modif

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,4 @@
-// controllers/auth.js
-const { verifyToken } = require('../middleware/token');
+
 
 const express = require("express");
 const router = express.Router();
@@ -7,7 +6,7 @@ const AuthController =require ("../controllers/auth")
 
 router.route("/register").post(AuthController.register);
 router.route("/update").post(AuthController.updateUserByEmail);
-router.route("/getuser").get(verifyToken,AuthController.getUserByEmail);
-
+router.route("/getuser").get(AuthController.getUserByEmail);
+router.route("/sign-in").post(AuthController.signIn);
 
 module.exports = router;
