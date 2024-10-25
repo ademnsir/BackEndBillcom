fc108e51aadf1d11c404889e52268a8c6eea2a66
ademnsir <nsir.adem@esprit.tn>
2024-10-26 00:07:24 +0100

    project  v1.0.1

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,32 +15,32 @@ exports.addProduct = async (req, res) => {
     try {
       const { title, prix, marque, dispo, promo, type, subcategory } = req.body;
   
-      // Check if the product image and logo have been uploaded
+      // Vérifier si les fichiers image et logo sont bien téléchargés
       if (!req.files || !req.files['image'] || !req.files['logoUrl']) {
-        return res.status(400).json({ message: "Both product image and brand logo are required." });
+        return res.status(400).json({ message: "Les images du produit et du logo sont requises" });
       }
   
-      // Create a new product object
+      // Créer un nouveau produit avec les données reçues
       const product = new Product({
         title,
         prix,
         marque,
         dispo,
         promo,
-        type,  // Category type (e.g., "gaming")
-        subcategory, // Subcategory (e.g., "Headset")
-        image: req.files['image'][0].filename, // Save the filename for the product image
-        logoUrl: req.files['logoUrl'][0].filename, // Save the filename for the brand logo
+        type,  // Ajout du champ `type`
+        subcategory, // Ajout du champ `subcategory`
+        image: req.files['image'][0].filename, // Sauvegarder le nom de fichier de l'image
+        logoUrl: req.files['logoUrl'][0].filename, // Sauvegarder le nom de fichier du logo
+        user: req.body.user ? { idUser: req.body.user.idUser } : undefined // Champ facultatif pour l'utilisateur
       });
   
-      // Save the product to the database
+      // Enregistrer le produit dans la base de données
       await product.save();
-      res.status(201).json({ message: 'Product added successfully', product });
+      res.status(201).json({ message: 'Produit ajouté avec succès', product });
     } catch (error) {
-      res.status(500).json({ message: "Error while adding the product", error });
+      res.status(500).json({ message: "Erreur lors de l'ajout du produit", error });
     }
   };
-  
   
   
   
