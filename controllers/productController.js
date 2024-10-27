93ac73722ed05288cde09acba642f820bed669fa
ademnsir <nsir.adem@esprit.tn>
2024-10-27 16:32:59 +0100

    project  v1.0.1

--- controllers/productController.js
+++ controllers/productController.js
@@ -49,15 +49,21 @@ exports.addProduct = async (req, res) => {
 // Récupérer un produit par ID
 exports.getProductById = async (req, res) => {
   try {
+    if (!req.params.id) {
+      return res.status(400).json({ message: 'ID manquant dans la requête' });
+    }
+
     const product = await Product.findById(req.params.id);
     if (!product) {
       return res.status(404).json({ message: 'Produit non trouvé' });
     }
+
     res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: 'Erreur lors de la récupération du produit', error });
   }
 };
+
 
 // Mettre à jour un produit
 exports.updateProduct = async (req, res) => {
