d09b123fa7281f954ee8d90476b178fd0ad149c4
ademnsir <nsir.adem@esprit.tn>
2024-10-28 01:46:41 +0100

    project  v1.0.4

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -3,28 +3,29 @@ const Review = require('../models/review');
 // Add a new review
 exports.addReview = async (req, res) => {
     try {
-        const { name, comment, priceRating, valueRating, qualityRating, user, product } = req.body;
-
-        // Create new review object
-        const review = new Review({
-            name,
-            comment,
-            priceRating,
-            valueRating,
-            qualityRating,
-            user,
-            product,
-            imgreview1: req.files['imgreview1'] ? req.files['imgreview1'][0].filename : null,
-            imgreview2: req.files['imgreview2'] ? req.files['imgreview2'][0].filename : null,
-        });
-
-        // Save the review to the database
-        await review.save();
-        res.status(201).json({ message: 'Review added successfully', review });
+      const { name, comment, priceRating, valueRating, qualityRating, user, product } = req.body;
+      console.log(req.body); // To check if data is being received correctly
+      
+      const review = new Review({
+        name,
+        comment,
+        priceRating,
+        valueRating,
+        qualityRating,
+        user,
+        product,
+        imgreview1: req.files['imgreview1'] ? req.files['imgreview1'][0].filename : null,
+        imgreview2: req.files['imgreview2'] ? req.files['imgreview2'][0].filename : null,
+      });
+      
+      await review.save();
+      res.status(201).json({ message: 'Review added successfully', review });
     } catch (error) {
-        res.status(500).json({ message: 'Error adding review', error });
+      console.error("Error adding review: ", error);
+      res.status(500).json({ message: 'Error adding review', error });
     }
-};
+  };
+  
 
 // Get reviews for a specific product
 exports.getReviewsByProduct = async (req, res) => {
