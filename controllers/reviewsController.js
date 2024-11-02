aa6c27669cf7485571c0709b4a3dab85412b166b
ademnsir <nsir.adem@esprit.tn>
2024-11-02 19:45:33 +0100

    project  v1.2.1

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -42,11 +42,9 @@ exports.addReview = async (req, res) => {
 exports.getReviewsByProduct = async (req, res) => {
   try {
     const { productId } = req.params;
-    // Ajouter populate pour charger les informations de l'utilisateur
-    const reviews = await Review.find({ product: productId }).populate('user');
+    const reviews = await Review.find({ product: productId }); // Correction de la requête MongoDB et ajout de `populate`
     res.status(200).json(reviews);
   } catch (error) {
-    console.error('Error fetching reviews:', error);
     res.status(500).json({ message: 'Error fetching reviews', error });
   }
 };
